af666088c24f106c9a6e62de778c8151810c58ff
Rakshit0404 <saritasakshi52@gmail.com>
2022-05-26 12:25:51 +0530

    hii

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
 const app = express();
+const io = require("socket.io");
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send("hello");
 })
 
 app.listen(3000, () => {
-    console.log("lisitening to port 3000")
+    console.log("lisitening to port 3000");
 });

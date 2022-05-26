956c1b128475da4114f0cd672faebc3d3dd7a347
Rakshit0404 <saritasakshi52@gmail.com>
2022-05-26 21:14:39 +0530

    hello

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const ejsmate = require('ejs-mate');
 const io = require("socket.io");
+const methodOverride = require("method-override");
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.engine('ejs', ejsmate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'views/styles')));
 
 app.get('/', (req, res) => {
-    res.send("hello");
-})
+    // res.render("pages/home");
+    res.render("pages/classes");
+});
+
+app.get('/:classid', (req, res)=>{
+    const classid=req.params.classid;
+    res.render("pages/class", {classid});
+});
 
 app.listen(3000, () => {
     console.log("lisitening to port 3000");
